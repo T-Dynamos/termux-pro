ec819715587593c13afc702ad2d0fcbddd800c15
T-Dynamos <68729523+T-Dynamos@users.noreply.github.com>
2021-06-11 16:25:45 +0530

    Add files via upload

---
+++ c.py
@@ -0,0 +1,24 @@
+import colorama
+import os
+from colorama import Fore,Back,Style
+print()
+os.system('toilet -f pagga "Success" | lolcat')
+print()
+print("🇨​​​​​🇴​​​​​🇲​​​​​🇲​​​​​🇦​​​​​🇳​​​​​🇩​​​​​🇸​​​​​")
+print()
+c = Fore.GREEN + Style.BRIGHT
+b = Fore.CYAN + Style.BRIGHT
+print(c +"[1] rsf  " , b + "          (to start Routersploit)")
+print(c + "[2] msf  "  , b + "          (to start Metasploit-Framework)")
+print(c + "[3] mbot "  ,b + "          (to start Maths Bot)")
+print(c + "[4] hack "   , b + "          (to start  AdvPhishing)")
+print(c + "[5] exploit ", b + "       (to start HiddenEye)")
+print(c + "[6] target -t <ip> ", b + "(to trace ip location)")
+print(c + "[7] target -m ", b + "     (to trace your ip)")
+print()
+print("🇳​​​​​🇴​​​​​🇼​​​​​ 🇼​​​​​🇭​​​​​🇦​​​​​🇹​​​​​ 🇹​​​​​🇴​​​​​ 🇩​​​​​🇴​​​​​")
+print()
+print(c)
+print("[1] Write Ngrok AuthToken ")
+print("[2] Press Ctrl+D or Logout")
+print()
