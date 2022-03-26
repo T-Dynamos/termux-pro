83b6c206c3710fa3c05674c62fff4e7e7f5a4889
Ansh Dadwal <68729523+T-Dynamos@users.noreply.github.com>
2022-03-26 18:46:54 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 echo "Installing Packages"
 apt update 
 apt install x11-repo -y
-apt install neovim fish neofetch xfce4* tigervnc -y 
+apt install neovim fish neofetch -y 
 echo "Installing Configrations"
 mkdir ~/.config > /dev/null 2>&1
 rm -rf  ~/.config && mv .config ~/.config
