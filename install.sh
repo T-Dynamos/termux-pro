c4185bd7c3f54c95dbe786280a4f8cd8c62f6713
T-Dynamos <anshdadwal298@gmail.com>
2022-03-26 09:50:05 +0530

    Added configs

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 echo "Installing Packages"
 apt update 
 apt install x11-repo -y
-apt install neovim fish neofetch xfce4* -y 
+apt install neovim fish neofetch xfce4* tigervnc -y 
 echo "Installing Configrations"
 mkdir ~/.config > /dev/null 2>&1
 rm ~/.config && mv .config ~/.config
