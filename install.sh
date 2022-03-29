b63b776c689bf4cc38437a7899d13b2e51102f03
Ansh Dadwal <68729523+T-Dynamos@users.noreply.github.com>
2022-03-29 09:44:17 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,12 @@ echo "Installing Packages"
 apt update 
 apt install x11-repo -y
 apt install neovim fish neofetch -y 
+curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+fisher install IlanCosman/tide@v5
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 echo "Installing Configrations"
 mkdir ~/.config > /dev/null 2>&1
 rm -rf  ~/.config && mv .config ~/.config
+chsh -s fish
 echo "Installed Success"
