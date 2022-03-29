258c31391d4fde0326b545dd15e77132f7621774
Ansh Dadwal <68729523+T-Dynamos@users.noreply.github.com>
2022-03-29 12:09:24 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ install_termuxpro () {
 	echo ""
 	apt update 
 	apt install x11-repo -y
-	apt install neovim fish git neofetch -y 
+	apt install neovim fish git tmux neofetch -y 
 	sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
 		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 	echo ""
